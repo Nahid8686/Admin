1becdc87ec9748488128a1ca9ca43169df48d5c8
AadityaBodke <aaditya.bodke0601@gmail.com>
2021-09-25 21:42:31 +0530

    trial commit

--- backend-master/app.js
+++ backend-master/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const url = 'mongodb://localhost/genera'
+const alienRouter=require('./routers/datamed')
 const app = express()
 
 // mongoose.connect(url, {useNewUrlParser:true})
@@ -11,7 +12,6 @@ const app = express()
 // })
 app.use(express.json())
 
-const alienRouter=require('./routers/datamed')
 app.use('/datamed',alienRouter)
 
 // app.listen(9000, () => {
